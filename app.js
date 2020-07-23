87baae94fc710ee8c02dbe6b4d3d45ee856e9950
Andrea Gallo <andreagallo264@gmail.com>
2020-07-23 14:27:56 -0300

    update port

--- app.js
+++ app.js
@@ -17,5 +17,22 @@ app.get('/detail', function (req, res) {
 app.use(express.static('assets'));
  
 app.use('/assets', express.static(__dirname + '/assets'));
- 
-app.listen(3000);
+const port = normalizePort(process.env.PORT || '3000'); 
+app.set('port', port);
+app.listen(port);
+
+function normalizePort(val) {
+    var port = parseInt(val, 10);
+  
+    if (isNaN(port)) {
+      // named pipe
+      return val;
+    }
+  
+    if (port >= 0) {
+      // port number
+      return port;
+    }
+  
+    return false;
+  }
