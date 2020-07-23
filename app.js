eea91d98ee59fc398c79760c2fc83313a0c89456
Andrea Gallo <andreagallo264@gmail.com>
2020-07-23 17:31:02 -0300

    console log

--- app.js
+++ app.js
@@ -86,7 +86,7 @@ app.post('/payment', (req, res, next) => {
     mercadopago.preferences.create(preference)
         .then(function (response) {
             // Este valor reemplazará el string "$$init_point$$" en tu HTML
-            console.log()
+            console.log(response);
             res.redirect(response.body.init_point);
         }).catch(function (err) {
             next(err);
